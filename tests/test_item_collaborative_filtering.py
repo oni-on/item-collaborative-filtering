3c56ae256ca281fee0ecadb9ed4873ec91452c59
oni <oni-on@users.noreply.github.com>
2018-11-19 16:53:55 +0100

    added test for helper method df_to_dict

---
+++ tests/test_item_collaborative_filtering.py
@@ -0,0 +1,24 @@
+import unittest
+
+import pandas as pd
+
+from src.item_collaborative_filtering import ItemItemCollaborativeFiltering
+
+class TestItemItemCollaborativeFiltering(unittest.TestCase):
+
+    def setUp(self):
+        self.item_item_collaborative_filtering = ItemItemCollaborativeFiltering()
+
+    def test_df_to_dict(self):
+        df = pd.DataFrame({'key': ['A', 'A', 'B', 'C', 'B'],
+                           'value': [1, 2, 3, 3, 5]})
+
+        self.assertEqual(
+            self.item_item_collaborative_filtering.df_to_dict(df, 'key', 'value'),
+            {'A': frozenset([1, 2]), 'B': frozenset([3, 5]), 'C': frozenset([3])}
+        )
+
+    def tearDown(self):
+        pass
+
+
