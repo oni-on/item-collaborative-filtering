88d1a6de0d7421b6b7108f59c04dccd7af142e78
oni-on <7388246+oni-on@users.noreply.github.com>
2018-11-26 14:39:02 +0100

    test fit_recommendations(), delete df_to_dict() test

--- tests/test_item_collaborative_filtering.py
+++ tests/test_item_collaborative_filtering.py
@@ -1,5 +1,6 @@
 import unittest
 
+import numpy as np
 import pandas as pd
 
 from src.item_collaborative_filtering import ItemItemCollaborativeFiltering
@@ -9,14 +10,34 @@ class TestItemItemCollaborativeFiltering(unittest.TestCase):
     def setUp(self):
         self.item_item_collaborative_filtering = ItemItemCollaborativeFiltering()
 
-    def test_df_to_dict(self):
-        df = pd.DataFrame({'key': ['A', 'A', 'B', 'C', 'B'],
-                           'value': [1, 2, 3, 3, 5]})
+    def test_fit_recommendations(self):
+        df = pd.DataFrame({'item_id': ['A', 'A', 'A', 'A', 'B', 'B', 'B'],
+                           'user_id': [1, 2, 3, 4, 1, 2, 5]})
 
-        self.assertEqual(
-            self.item_item_collaborative_filtering.df_to_dict(df, 'key', 'value'),
-            {'A': frozenset([1, 2]), 'B': frozenset([3, 5]), 'C': frozenset([3])}
+        recommendations = self.item_item_collaborative_filtering.fit_recommendations(df)
+
+        self.assertTrue(
+            (recommendations.item.values==np.array(['A', 'B'])).all(),
+            msg="Recommendations dataframe 'item' column values not as expected"
+        )
+        self.assertTrue(
+            (recommendations.recommended_item.values==np.array(['B', 'A'])).all(),
+            msg="Recommendations dataframe 'recommended_item' column values not as expected"
+        )
+        self.assertTrue(
+            (recommendations.count_common_users.values==np.array([2, 2])).all(),
+            msg="__count_common_item_pair_users() - unexpected results"
+        )
+        self.assertTrue(
+            (recommendations.expected_common_users.values==np.array([6/5, 8/5])).all(),
+            msg="__expected_common_item_pair_users() - unexpected results"
+        )
+        self.assertTrue(
+            (recommendations.score.values.round(4)==np.array([0.5418, 0.2346])).all(),
+            msg="recommendations scores - unexpected results"
         )
 
     def tearDown(self):
         pass
+
+
