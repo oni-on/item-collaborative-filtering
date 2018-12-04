df62ed487b4e76d2fc9e208b14fa1cba838aa9a1
oni-on <7388246+oni-on@users.noreply.github.com>
2018-12-04 16:57:09 +0100

    clean up timing functions and further performance optimisation

--- src/item_collaborative_filtering.py
+++ src/item_collaborative_filtering.py
@@ -9,15 +9,6 @@ from joblib import Parallel, delayed
 import numpy as np
 import pandas as pd
 
-def timefunc(f):
-    def f_timer(*args, **kwargs):
-        start = time.time()
-        result = f(*args, **kwargs)
-        end = time.time()
-        print (f.__name__, 'took', end - start, 'sec')
-        return result
-    return f_timer
-
 
 class ItemItemCollaborativeFiltering:
 
@@ -38,7 +29,6 @@ class ItemItemCollaborativeFiltering:
             return [(item, paired_item) for
                     item, paired_item in permutations(df[self.item_column].unique(), 2)]
 
-    @timefunc
     def __item_users(self, df):
         """
         Looks up the users that interacted (e.g. watched, purchased) with an item
@@ -46,15 +36,16 @@ class ItemItemCollaborativeFiltering:
         :return: dictionary {item: set(user1, user2, ...)}
         """
 
-        item_users = {
-            # set of item users
-            item: set(df.loc[df[self.item_column] == item, self.user_column])
-            for item in df[self.item_column].unique()
-        }
+        item_users = df.groupby(
+            self.item_column
+        ).agg(
+            {
+                self.user_column: lambda x: set(x)
+            }
+        ).to_dict()[self.user_column]
 
         self.item_users = item_users
 
-    @timefunc
     def __count_common_item_pair_users(self, item_pair):
         """
         Computes the number of users that interacted (e.g. watched, purchased) with BOTH items in a pair
@@ -70,22 +61,22 @@ class ItemItemCollaborativeFiltering:
         except AttributeError:
             "Extract item users first, using __item_users()"
 
-    @timefunc
     def __item_probabilities(self, df):
         """
         Computes the probability of a user interacting with an item (e.g. watching, purchasing, liking)
         :param df:  dataframe with columns [user_id, item_id]
         :return: dictionary with key=item_id, value = interaction probability (float)
         """
-
-        item_probabilities = {
-            item: df.loc[df[self.item_column] == item, self.user_column].nunique()/df[self.user_column].nunique()
-            for item in df[self.item_column].unique()
-        }
+        item_probabilities = (
+                df.groupby(
+                    self.item_column
+                ).agg(
+                    {self.user_column: 'nunique'}
+                )/df[self.user_column].nunique()
+        ).to_dict()[self.user_column]
 
         self.item_probabilities = item_probabilities
 
-    @timefunc
     def __item_interaction_probability(self, item):
         """
         Looks up the probability of a user interacting with an item (e.g. watching, purchasing, liking)
@@ -97,7 +88,6 @@ class ItemItemCollaborativeFiltering:
         except AttributeError:
             "Extract item probabilities first, using __item_probabilities()"
 
-    @timefunc
     def __user_interactions(self, df):
         """
         For ALL users computes the number of items that every user interacted with, APART from item in the df row
@@ -112,7 +102,6 @@ class ItemItemCollaborativeFiltering:
 
         self.user_interactions = interactions_count.to_dict()
 
-    @timefunc
     def __count_users_interactions(self, df, item):
         """
         For ALL users of an item, it looks up the number of other items that every user interacted with, APART from item
@@ -128,7 +117,6 @@ class ItemItemCollaborativeFiltering:
         except AttributeError:
             "Extract user interactions first, using __user_interactions()"
 
-    @timefunc
     def __expected_common_item_pair_users(self, df, item_pair):
         """
         item_pair consists of (item1, item2)
@@ -173,7 +161,6 @@ class ItemItemCollaborativeFiltering:
         """
         return (actual_users - expected_users) * np.log(actual_users + 0.1) / np.sqrt(expected_users)
 
-    @timefunc
     def fit_recommendations(self, df, item=None, processes=2):
         """
         Computes recommendation strength for item pairs
@@ -186,6 +173,8 @@ class ItemItemCollaborativeFiltering:
         item_pairs = self.__generate_item_pairs(df, item)
 
         self.__item_users(df)
+        self.__item_probabilities(df)
+        self.__user_interactions(df)
 
         # output: [((item1, item2), common_users)]
         count_pair_users = Parallel(n_jobs=processes)(
@@ -202,9 +191,6 @@ class ItemItemCollaborativeFiltering:
 
         # extract item pair, and user count
         filtered_item_pairs, count_pair_users = zip(*count_pair_users)
-
-        self.__item_probabilities(df)
-        self.__user_interactions(df)
 
         # output: [expected_users]
         # compute expected users for item pairs with at least 1 user in common
